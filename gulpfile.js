86e824e2329acea6fd9b5ce91cd70f802e6cfac3
Антон <bozhan89@gmail.com>
2018-05-09 13:46:23 +0300

    Начал подготавливать сборку проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp rename")
+var del = require("del");
 var server = require("browser-sync").create();
 
 gulp.task("style", function() {
@@ -14,8 +17,26 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build")
 });
 
 gulp.task("serve", ["style"], function() {
