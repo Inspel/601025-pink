7d1f6dc10541f66abd7bcbfc76fc4b79ce7d7897
Антон <bozhan89@gmail.com>
2018-05-14 11:09:32 +0300

    Внес правки после первой проверки

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-var navigation = document.querySelector('.navigation');
-var toggle = document.querySelector('.navigation__nav-btn');
+var navigation = document.querySelector(".navigation");
+var toggle = document.querySelector(".navigation__nav-btn");
 
-navigation.classList.remove('navigation--nojs');
-navigation.classList.add('navigation--closed');
+navigation.classList.remove("navigation--nojs");
+navigation.classList.add("navigation--closed");
 
-toggle.addEventListener('click', function () {
-  if (navigation.classList.contains('navigation--closed')) {
-    navigation.classList.remove('navigation--closed');
+toggle.addEventListener("click", function () {
+  if (navigation.classList.contains("navigation--closed")) {
+    navigation.classList.remove("navigation--closed");
   } else {
-    navigation.classList.add('navigation--closed');
+    navigation.classList.add("navigation--closed");
   }
 });
