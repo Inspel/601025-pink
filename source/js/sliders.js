1f5b7b9abbfbd49818e608c50244b188636e635b
Антон <bozhan89@gmail.com>
2018-06-25 15:11:52 +0300

    Поправил слайдеры

--- source/js/sliders.js
+++ source/js/sliders.js
@@ -1,51 +1,77 @@
 "use strict";
+(function () {
 
-var start_reviews = 0;
+  var start_reviews = 0;
+  var width_reviews;
+  var list_reviews = document.querySelector(".slider__list--reviews");
+  var toggles_reviews = document.querySelectorAll(".slider__toggle--reviews");
 
-var width_reviews = 320;
-
-var list_reviews = document.querySelector(".slider__list--reviews");
-
-var toggles_reviews = document.querySelectorAll(".slider__toggle--reviews");
-
-var _loop = function _loop(i) {
-  toggles_reviews[i].onclick = function () {
-
-    start_reviews = i;
+  var on_toggle_click = function (event) {
+    width_reviews = window.innerWidth;
+    var clicked_toggle = event.target;
+    start_reviews = clicked_toggle.getAttribute('index');
     list_reviews.style.left = -(start_reviews * width_reviews) + "px";
-    console.log(toggles_reviews.length);
-    for (var j = 0; j < toggles_reviews.length; j++) {
-      toggles_reviews[j].classList.remove("slider__toggle--active");
+    for (var i = 0; i < toggles_reviews.length; i++) {
+      toggles_reviews[i].classList.remove("slider__toggle--active");
     }
     toggles_reviews[start_reviews].classList.add("slider__toggle--active");
   };
-};
 
-for (var i = 0; i < toggles_reviews.length; i++) {
-  _loop(i);
-}
+  toggles_reviews.forEach(function (currentValue, i) {
+    currentValue.setAttribute("index", i.toString());
+    currentValue.addEventListener("click", function (event) {
+      on_toggle_click(event)
+    })
+  });
 
-var start_tariffs = 0;
+  var arrows = document.querySelector(".slider__arrows-wrapper");
+  var review_count = 0;
 
-var width_tariffs = 280;
-
-var list_tariffs = document.querySelector(".slider__list--tariffs");
-
-var toggles_tariffs = document.querySelectorAll(".slider__toggle--tariffs");
-
-var _loop2 = function _loop2(i) {
-  toggles_tariffs[i].onclick = function () {
-
-    start_tariffs = i;
-    list_tariffs.style.left = -(start_tariffs * width_tariffs) + "px";
-    console.log(toggles_tariffs.length);
-    for (var j = 0; j < toggles_tariffs.length; j++) {
-      toggles_tariffs[j].classList.remove("slider__toggle--active");
+  var on_arrows_click = function (event) {
+    var target = event.target;
+    if (target && target.parentNode === arrows.firstElementChild) {
+      review_count--;
+      if (review_count < 0) {
+        review_count = 0;
+      }
+    } else if (target && target.parentNode === arrows.lastElementChild) {
+      review_count++;
+      if (review_count > list_reviews.childElementCount) {
+        review_count = list_reviews.childElementCount;
+      }
     }
-    toggles_tariffs[start_tariffs].classList.add("slider__toggle--active");
+    console.log(review_count);
+    width_reviews = window.innerWidth;
+    list_reviews.style.left = review_count * width_reviews + "px";
   };
-};
 
-for (var i = 0; i < toggles_tariffs.length; i++) {
-  _loop2(i);
-}
+  arrows.addEventListener('click', function (event) {
+    on_arrows_click(event);
+  });
+})();
+
+(function () {
+  var start_tariffs;
+
+  var width_tariffs = 280;
+
+  var list_tariffs = document.querySelector(".slider__list--tariffs");
+
+  var toggles_tariffs = document.querySelectorAll(".slider__toggle--tariffs");
+
+  var _loop2 = function _loop2(i) {
+    toggles_tariffs[i].onclick = function () {
+
+      start_tariffs = i;
+      list_tariffs.style.left = -(start_tariffs * width_tariffs) + "px";
+      for (var j = 0; j < toggles_tariffs.length; j++) {
+        toggles_tariffs[j].classList.remove("slider__toggle--active");
+      }
+      toggles_tariffs[start_tariffs].classList.add("slider__toggle--active");
+    };
+  };
+
+  for (var i = 0; i < toggles_tariffs.length; i++) {
+    _loop2(i);
+  }
+})();
